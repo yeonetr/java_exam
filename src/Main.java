2f5b586decb2d2cc7b8c030aba8c86f643224df3
yeonetr <youj8419@gmail.com>
2023-08-30 14:16:01 +0900

    ìexam1 컷밋

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // 숫자 출력
+        System.out.println(100);
+
+        // 문자 출력
+        System.out.println("hello");
+
+    }
+}
